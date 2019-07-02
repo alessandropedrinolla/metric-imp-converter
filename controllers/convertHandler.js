6aed43153510d8197f7a3662b4d91870d19f4d34
Alessandro Pedrinolla <a.pedrinolla@gmail.com>
2019-07-02 23:14:52 +0200

    done

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,43 +6,99 @@
 *       
 */
 
+let unitsString = {
+  "gal": "gallons",
+  "lbs": "libres",
+  "mi": "miles",
+  "L": "liters",
+  "kg": "kilograms",
+  "km": "kilometers",
+}
+
 function ConvertHandler() {
   
-  this.getNum = function(input) {
-    var result;
+  this.getNum = function(input) {    
+    var result = input.match(/.*(?=(gal|lbs|mi|L|kg|km)$)/);
     
-    return result;
+    if(result)
+    {
+        result = result[0];
+
+        if(result == "")
+          return 1;
+
+        console.log("getnum res: " + result);
+
+        // If it's not a number
+        if(!/^[0-9]*(\.[0-9]+)?(\/[0-9]+(\.[0-9]+)?)?$/.test(result)) 
+          return NaN;
+
+        // If contains a '/'
+        if(result.indexOf('/')!=-1)
+        {
+          let parts = result.split('/');
+          return parseFloat(parts[0]) / parseFloat(parts[1]);
+        }
+        return result;
+    }
+    else
+      return NaN;
   };
-  
+
   this.getUnit = function(input) {
     var result;
-    
-    return result;
+    result = input.match(/(gal|lbs|mi|L|kg|km)$/);
+
+    if(result && unitsString[result[0]])
+      return result[0];
+    return -1;
   };
   
   this.getReturnUnit = function(initUnit) {
+    let units = ["gal","lbs","mi","L","kg","km"];
     var result;
-    
+    switch(initUnit)
+    {
+      case "gal":case "lbs":case "mi": 
+      {
+        result = units[units.indexOf(initUnit) + 3];
+      }; break;
+      case "L":case "kg":case "km": {
+        result = units[units.indexOf(initUnit) - 3];
+      }; break;
+    }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
+    return unitsString[unit];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
+
+    let ops = {
+      "gal": (num) => { return num * galToL; },
+      "lbs": (num) => { return num * lbsToKg; },
+      "mi": (num) => { return num * miToKm; },
+      "L": (num) => { return num / galToL; },
+      "kg": (num) => { return num / lbsToKg; },
+      "km": (num) => { return num / miToKm; },
+    }
+
+    console.log("convert: initunit:" + initUnit + " initnum:" + initNum);
+
+    var result = ops[initUnit](initNum);
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     var result;
+
+    result = `${initNum} ${unitsString[initUnit]} converts to ${returnNum} ${unitsString[returnUnit]}`;
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,11 @@
 
 var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
+var helmet      = require('helmet');
 
 module.exports = function (app) {
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.xssFilter());
   
   var convertHandler = new ConvertHandler();
 
@@ -20,11 +23,36 @@ module.exports = function (app) {
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
+
+      // Check invalid input
+
+      let err = "";
+
+      console.log("api.js initUnit:"+ initUnit+" initnum:" + initNum);
+
+      if(Number.isNaN(initNum))
+      {
+        err = "invalid number";
+        if(initUnit == -1)
+        {
+          err = "invalid number and unit";
+        }
+      }else
+      {
+        if(initUnit == -1)
+          err = "invalid unit";
+      }
+
+      if(err!="")
+      {
+        res.json({error:err});
+        return;
+      }
+
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
-      //res.json
+      res.json({"initNum": initNum, "initUnit": initUnit, "returnNum": returnNum, "returnUnit": returnUnit, "string": toString});
     });
-    
 };
